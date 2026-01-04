885b194078db3c06adca15e6d99fc41b87f9e8b5
akvigi <akvigi.work@gmail.com>
2026-01-04 22:38:44 +0200

    reorganize navigation setup and enhance module imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,7 @@
 import { StatusBar, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createStaticNavigation } from '@react-navigation/native';
-import { StaticParamList } from '@react-navigation/core';
-import ProfileScreen from './screens/ProfileScreen.tsx';
-import HomeScreen from './screens/HomeScreen.tsx';
-import useColors from "./hooks/useColors";
-
-const RootStack = createNativeStackNavigator({
-  screens: {
-    Home: HomeScreen,
-    Profile: ProfileScreen,
-  },
-});
-
-// bun add/npm i @react-navigation/bottom-tabs
-// const BottomRootStack = createBottomTabNavigator({
-//   screens: {
-//     Home: HomeScreen,
-//     Profile: ProfileScreen,
-//   },
-// });
-
-type RootStackParamList = StaticParamList<typeof RootStack>;
-
-declare global {
-  namespace ReactNavigation {
-    interface RootParamList extends RootStackParamList {}
-  }
-}
-
-const Navigation = createStaticNavigation(RootStack);
+import useColors from './hooks/useColors';
+import { Navigation } from './navigation/base.ts';
 
 function App() {
   const colors = useColors();
---
+++ src/navigation/base.ts
@@ -0,0 +1,32 @@
+import { createStaticNavigation } from '@react-navigation/native';
+import { StaticParamList } from '@react-navigation/core';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '@screens/HomeScreen.tsx';
+import ProfileScreen from '@screens/ProfileScreen.tsx';
+
+const RootStack = createNativeStackNavigator({
+  screens: {
+    Home: HomeScreen,
+    Profile: ProfileScreen,
+  },
+});
+
+// bun add/npm i @react-navigation/bottom-tabs
+// const BottomRootStack = createBottomTabNavigator({
+//   screens: {
+//     Home: HomeScreen,
+//     Profile: ProfileScreen,
+//   },
+// });
+
+type RootStackParamList = StaticParamList<typeof RootStack>;
+
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
+
+const Navigation = createStaticNavigation(RootStack);
+
+export { Navigation };
